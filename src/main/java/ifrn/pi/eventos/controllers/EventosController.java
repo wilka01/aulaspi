07b317f5d4e6b3d8323e1edecbc4e24b791ff49d
wilka <wilkawilkakelly5@gmail.com>
2023-09-25 23:26:41 -0300

    Criando páginas de detalhes do evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -2,9 +2,11 @@ package ifrn.pi.eventos.controllers;
 
 import java.util.List;
 
+import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,11 +14,10 @@ import org.springframework.web.servlet.ModelAndView;
 import ifrn.pi.eventos.models.Evento;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
-
 @Controller
-@RequestMapping ("/eventos")
+@RequestMapping("/eventos")
 public class EventosController {
-	
+
 	@Autowired
 	private EventoRepository er;
 
@@ -24,22 +25,38 @@ public class EventosController {
 	public String form() {
 		return "eventos/formEvento";
 	}
-	
+
 	@PostMapping()
 	public String adicionar(Evento evento) {
-		
+
 		System.out.println(evento);
 		er.save(evento);
-		
-	return "eventos/evento-adicionado";
+
+		return "eventos/evento-adicionado";
 	}
-	
-	@GetMapping 
+
+	@GetMapping
 	public ModelAndView listar() {
 		List<Evento> eventos = er.findAll();
 		ModelAndView mv = new ModelAndView("eventos/lista");
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
-}
 
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		java.util.Optional<Evento> opt = er.findById(id);
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/eventos");
+			return md;
+		}
+
+		md.setViewName("eventos/detalhes");
+		Evento evento = opt.get();
+
+		md.addObject("evento", evento);
+		return md;
+
+	}
+}
